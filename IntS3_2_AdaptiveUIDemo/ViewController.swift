78444449bf0e9e6bba9b6b989795ece2984901f0
Vlado Velkovski <vladov@vlados-mac.local>
2017-01-20 05:10:52 -0800

    Initial Commit

---
+++ IntS3_2_AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntS3_2_AdaptiveUIDemo
+//
+//  Created by Vlado Velkovski on 1/20/17.
+//  Copyright © 2017 Vlado Velkovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
